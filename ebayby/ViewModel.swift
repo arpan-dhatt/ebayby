8f9f80f6b83ce8205c58fbfa20793795c4c479f8
saakethc <s@gmail.com>
2021-01-16 16:05:22 -0600

    added a base viewModel

---
+++ ebayby/ViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ViewModel.swift
+//  ebayby
+//
+//  Created by user175571 on 1/16/21.
+//
+
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    @Published var model: InfoModel = InfoModel()
+    
+    //Constants
+    let PrimaryColor = Color.init(red: 238/255, green: 174/255, blue: 202/255)
+    let SecondaryColor = Color.init(red: 238/255, green: 174/255, blue: 202/255)
+    
+    //Transistion Variables
+    @Published var page = "login"
+    @Published var currentOrderView = "Overview"
+    
+    //User
+    @Published var user: InfoModel.User = InfoModel.User.init(name: "Rishik", BabiesInCart: [], BabiesOrdered: [])
+    
+    
+    //BabyBase Infortion
+    @Published var AllBases: Array<InfoModel.BabyBase> = [
+        .init(NameOfCeleb: "Donnald", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald1", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald2", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald3", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald4", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald5", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald6", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald7", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+        .init(NameOfCeleb: "Donnald8", ImageOfCeleb: UIImage(named: "Donlad")!, BasePrice: 1, Description:"nulllll"),
+    ]
+}
