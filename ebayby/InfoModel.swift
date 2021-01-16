d957d01e6ace8a8a1d1907c9957e99d6153d8747
saakethc <s@gmail.com>
2021-01-16 15:43:04 -0600

    made preliminary info model

--- ebayby/InfoModel.swift
+++ ebayby/InfoModel.swift
@@ -6,9 +6,68 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct InfoModel{
-    struct Baby{
-        
+    
+    struct BabyBase{
+        var NameOfCeleb: String
+        var ImageOfCeleb: UIImage
+        var BasePrice: Float
+        var Description: String
     }
+    struct Article{
+        var Title: String
+        var Image: UIImage
+        var Subtitle: String
+        var Description: String
+    }
+    struct Accessory{
+        var ImageOfAccessory: UIImage
+        var Name: String
+        var Price: Float
+        var Description: String
+    }
+    struct Baby{
+        var Base: BabyBase
+        var IQ: Int
+        var EyeColor: EyeColor
+        var SkinColor: SkinColor
+        var Height: Float
+        var Thiccnes: Float
+        var Traits: [String]
+        var TotalPrice: Float
+        var ProjectedIncome: Float
+    }
+    struct Order{
+        var OrderedBaby: Baby
+        var Financing: Financing
+        var Status: Status
+        var AdditionalAccessories: [Accessory]
+    }
+    struct User{
+        var name: String
+        var BabiesInCart: [Baby]
+        var BabiesOrdered: [Order]
+    }
+    
+    enum EyeColor{
+        case blue, black, white, brown, green
+    }
+    
+    enum SkinColor {
+        case white, brown, tan, black, red, blue
+    }
+    
+    enum Financing {
+        case loan, credit, debit, cash
+    }
+    
+    enum Status {
+        case paid, none, partiallyPaid
+    }
+    
+    
+    
+    
 }
