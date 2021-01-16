033e531cb5a85e51b4c19b5fa8f06b8668faf9de
saakethc <s@gmail.com>
2021-01-16 16:16:55 -0600

    added a main view to control which view is shown at any time

---
+++ ebayby/MainView.swift
@@ -0,0 +1,116 @@
+//
+//  MainView.swift
+//  ebayby
+//
+//  Created by user175571 on 1/16/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @ObservedObject var model: ViewModel = ViewModel()
+    
+    var body: some View {
+        VStack{
+            if model.page == "home"{
+                VStack{
+                    Spacer()
+                    Text("Home")
+                    Spacer()
+                    VStack{
+                        
+                        Button(action: {
+                            self.model.page = "order"
+                        }){
+                            Text("Orders").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "home"
+                        }){
+                            Text("Home").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "login"
+                        }){
+                            Text("Login").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                }
+            }
+            if model.page == "login"{
+                VStack{
+                    Spacer()
+                    Text("Login")
+                    Spacer()
+                    VStack{
+                        
+                        Button(action: {
+                            self.model.page = "order"
+                        }){
+                            Text("Orders").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "home"
+                        }){
+                            Text("Home").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "login"
+                        }){
+                            Text("Login").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                }
+            }
+            if model.page == "order"{
+                VStack{
+                    Spacer()
+                    Text("Order")
+                    Spacer()
+                    VStack{
+                        
+                        Button(action: {
+                            self.model.page = "order"
+                        }){
+                            Text("Orders").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "home"
+                        }){
+                            Text("Home").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                        
+                        Button(action: {
+                            self.model.page = "login"
+                        }){
+                            Text("Login").font(.system(size: 24, weight: .light)).padding()
+                        }.frame(width: 225).background(Color.green).cornerRadius(10.0).shadow(radius: 10.0).foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
