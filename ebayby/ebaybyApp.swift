04105ac0d6b347f6a556401df17ee0d7c4e48187
Arpan Dhatt <ary.dhatt@gmail.com>
2021-01-16 15:05:42 -0600

    Initial Commit

---
+++ ebayby/ebaybyApp.swift
@@ -0,0 +1,20 @@
+//
+//  ebaybyApp.swift
+//  ebayby
+//
+//  Created by Arpan Dhatt on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ebaybyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
