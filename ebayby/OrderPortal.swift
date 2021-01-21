93778f50f5bfc26c469ffb6d2ecd63368d36843e
SaakethC <saakethrchenna@gmail.com>
2021-01-20 22:05:04 -0600

    continued on order portal

--- ebayby/OrderPortal.swift
+++ ebayby/OrderPortal.swift
@@ -14,28 +14,58 @@ struct OrderPortal: View {
         ZStack{
             ScrollView{
                 VStack{
-                    HStack{
-                        Text("Your Babies").font(.system(size: 28, weight: .bold)).foregroundColor(Color.black).padding()
-                        Spacer()
-                        Image(systemName: "person.circle").font(.system(size: 28, weight: .bold)).foregroundColor(Color.black).padding()
-                    }
-
-                        if model.allOrders.count == 0{
-                            VStack{
-                                Text("You Have No Children").font(.system(size: 28, weight: .light)).foregroundColor(Color.black).padding()
-                                Text("You Have No Purpose").font(.system(size: 18, weight: .ultraLight)).foregroundColor(Color.black).padding()
-                                Text("- President Bethi").font(.system(size: 14, weight: .light)).foregroundColor(Color.black).padding()
-                            }
-                        }
-                        else{
-                            
-                        }
+                    coolTitle(text: "Your Babies", image: "person.circle")
                     
+                    if model.allOrders.count == 0{
+                        VStack{
+                            Spacer()
+                            Text("You Have No Children").font(.system(size: 28, weight: .light)).foregroundColor(Color.black).padding()
+                            Text("You Have No Purpose").font(.system(size: 18, weight: .ultraLight)).foregroundColor(Color.black).padding()
+                            Text("- President Bethi").font(.system(size: 14, weight: .light)).foregroundColor(Color.black).padding()
+                            Spacer()
+                        }
+                    }
+                    else{
+                        VStack{
+                            coolTitleLight(text: "In Delivery")
+                        }
+                        VStack{
+                            coolTitleLight(text: "In Creation")
+                        }
+                        VStack{
+                            coolTitleLight(text: "In Delivery")
+                        }
+                    }
                 }
             }
         }
     }
 }
+
+struct coolTitle: View {
+    var text: String
+    var image: String
+    
+    var body: some View {
+        HStack{
+            Text(text).font(.system(size: 28, weight: .bold)).foregroundColor(Color.black).padding()
+            Spacer()
+            Image(systemName: image).font(.system(size: 28, weight: .bold)).foregroundColor(Color.black).padding()
+        }
+    }
+}
+
+struct coolTitleLight: View {
+    var text: String
+    
+    var body: some View {
+        HStack{
+            Text(text).font(.system(size: 24, weight: .light)).foregroundColor(Color.black).padding()
+            Spacer()
+        }
+    }
+}
+
 
 struct OrderPortal_Previews: PreviewProvider {
     static var previews: some View {
