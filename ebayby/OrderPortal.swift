c2ea78fa53f52a800669a7ab0ed90ae328dcc24a
Arpan Dhatt <ary.dhatt@gmail.com>
2021-01-21 00:45:39 -0600

    made sorta matched geometry effect

--- ebayby/OrderPortal.swift
+++ ebayby/OrderPortal.swift
@@ -27,7 +27,7 @@ struct OrderPortal: View {
                     }
                     else{
                         VStack{
-                            coolTitleLight(text: "In Delivery")
+                            coolTitleLight(text: "Ordering")
                             ForEach(model.allOrders, id: \.id){ order in
                                 if order.Status == .delivery {
                                     deliveryCard(model: self.model, card:order).padding().shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
@@ -35,7 +35,7 @@ struct OrderPortal: View {
                             }
                         }
                         VStack{
-                            coolTitleLight(text: "In Creation")
+                            coolTitleLight(text: "In Transit")
                             ForEach(model.allOrders, id: \.id){ order in
                                 if order.Status == .ordering {
                                     deliveryCard(model: self.model, card:order).padding().shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
@@ -43,7 +43,7 @@ struct OrderPortal: View {
                             }
                         }
                         VStack{
-                            coolTitleLight(text: "In Delivery")
+                            coolTitleLight(text: "Delivered")
                             ForEach(model.allOrders, id: \.id){ order in
                                 if order.Status == .paid || order.Status == .partiallyPaid {
                                     deliveryCard(model: self.model, card:order).padding().shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
@@ -59,46 +59,113 @@ struct OrderPortal: View {
 
 struct deliveryCard: View {
     @ObservedObject var model: ViewModel
+    @Namespace var cardspace
+    @State private var expanded: Bool = false
     var card: InfoModel.Order
     
     var body: some View {
-        ZStack{
-            VStack{
-                HStack{
-                    Image(uiImage: card.OrderedBaby.Base.ImageOfCeleb).resizable().frame(width:100, height: 100).aspectRatio(contentMode: .fit).clipShape(Circle()).shadow(radius: 10).padding()
-                    VStack{
-                        Text(card.OrderedBaby.Base.NameOfCeleb).font(.system(size: 28, weight: .bold)).foregroundColor(Color.white)
+            ZStack{
+                VStack {
+                    if !expanded {
                         HStack{
-                            VStack(alignment: .leading){
-                                Text("IQ: "+String(card.OrderedBaby.IQ))
-                                Text("Eye Color: "+card.OrderedBaby.EyeColor.rawValue)
+                                Image(uiImage: card.OrderedBaby.Base.ImageOfCeleb).resizable().frame(width:100, height: 100).aspectRatio(contentMode: .fit).clipShape(Circle()).shadow(radius: 10).padding()
+                                VStack{
+                                    Text(card.OrderedBaby.Base.NameOfCeleb).font(.system(size: 28, weight: .bold)).foregroundColor(Color.white).matchedGeometryEffect(id: "name", in: cardspace)
+                                    Text("Delivery Status: "+card.Status.rawValue.uppercased()).font(.caption)
+                                    Text("Payment Method: "+card.Financing.rawValue.uppercased()).font(.caption)
+                                }.padding(.trailing)
+                        }
+                    }
+                    else {
+                        HStack{
+                                Image(uiImage: card.OrderedBaby.Base.ImageOfCeleb).resizable().frame(width:100, height: 100).aspectRatio(contentMode: .fit).clipShape(Circle()).shadow(radius: 10).padding()
+                            VStack(spacing: 20){
+                                Text(card.OrderedBaby.Base.NameOfCeleb).font(.system(size: 28, weight: .bold)).foregroundColor(Color.white).matchedGeometryEffect(id: "name", in: cardspace)
+                                    Text("Delivery Status: "+card.Status.rawValue.uppercased()).font(.caption).padding(.trailing).matchedGeometryEffect(id: "delivery status", in: cardspace)
                             }
-                            
-                            VStack(alignment: .leading){
-                                Text("Height: "+String(card.OrderedBaby.Height))
-                                Text("Skin Color: "+card.OrderedBaby.SkinColor.rawValue)
-                                
-                            }
-                        }.font(.caption2).padding(.top)
+                        }
+                        HStack {
+                            Image(systemName: "square.and.pencil").font(.system(size: 24, weight: .bold)).foregroundColor(Color.white)
+                            Text("IQ: ")
+                            Spacer()
+                            Text("\(card.OrderedBaby.IQ)")
+                        }
+                        HStack {
+                            Image(systemName: "ruler").font(.system(size: 18, weight: .bold)).foregroundColor(Color.white)
+                            Text("Height: ")
+                            Spacer()
+                            Text("\(Int(card.OrderedBaby.Height))cm")
+                        }
+                        HStack {
+                            Image(systemName: "pencil.tip.crop.circle.badge.plus").font(.system(size: 20, weight: .bold)).foregroundColor(Color.white)
+                            Text("Skin Color: ")
+                            Spacer()
+                            Text("\(card.OrderedBaby.SkinColor.rawValue.uppercased())")
+                        }
+                        HStack {
+                            Image(systemName: "eye.fill").font(.system(size: 18, weight: .bold)).foregroundColor(Color.white)
+                            Text("Eye Color: ")
+                            Spacer()
+                            Text("\(card.OrderedBaby.EyeColor.rawValue.uppercased())")
+                        }
+                        HStack {
+                            Image(systemName: "ruler").font(.system(size: 18, weight: .bold)).foregroundColor(Color.white)
+                            Text("Thiccness: ")
+                            Spacer()
+                            Text("\(Int(card.OrderedBaby.Thiccnes))%")
+                        }
+                        Spacer()
+                        HStack {
+                            Text("Payment Method: "+card.Financing.rawValue.uppercased()).font(.caption).matchedGeometryEffect(id: "payment method", in: cardspace)
+                            Spacer()
+                            Text("$\(Int(card.OrderedBaby.TotalPrice))")
+                        }
+
+                    }
+                }.padding()
+                .frame(width: UIScreen.main.bounds.width-30).background(Color.black).foregroundColor(Color.white).cornerRadius(10.0).matchedGeometryEffect(id: "card", in: cardspace).onTapGesture {
+                    withAnimation {
+                        expanded.toggle()
                     }
                 }
-                HStack{
-                    VStack(alignment: .leading){
-                        Text("Financing: "+card.Financing.rawValue)
-                        Text("Status: "+card.Status.rawValue)
-                        Spacer()
-                    }.padding([.leading,.top,.bottom]).font(.callout)
-                    
-                    VStack(alignment: .leading){
-                        Text("Total Price: $"+String(card.OrderedBaby.TotalPrice))
-                        Text("Projected Income: $"+String(card.OrderedBaby.ProjectedIncome))
-                        Text("Net Benefit: $"+String(card.OrderedBaby.TotalPrice - card.OrderedBaby.ProjectedIncome))
-                    }.padding(.trailing).font(.footnote)
-                }
-            }.background(Color.black).foregroundColor(Color.white).cornerRadius(10.0)
+            }
         }
+//        ZStack{
+//            VStack{
+//                HStack{
+//                    Image(uiImage: card.OrderedBaby.Base.ImageOfCeleb).resizable().frame(width:100, height: 100).aspectRatio(contentMode: .fit).clipShape(Circle()).shadow(radius: 10).padding()
+//                    VStack{
+//                        Text(card.OrderedBaby.Base.NameOfCeleb).font(.system(size: 28, weight: .bold)).foregroundColor(Color.white)
+//                        HStack{
+//                            VStack(alignment: .leading){
+//                                Text("IQ: "+String(card.OrderedBaby.IQ))
+//                                Text("Eye Color: "+card.OrderedBaby.EyeColor.rawValue)
+//                            }
+//
+//                            VStack(alignment: .leading){
+//                                Text("Height: "+String(card.OrderedBaby.Height))
+//                                Text("Skin Color: "+card.OrderedBaby.SkinColor.rawValue)
+//
+//                            }
+//                        }.font(.caption2).padding(.top)
+//                    }
+//                }
+//                HStack{
+//                    VStack(alignment: .leading){
+//                        Text("Financing: "+card.Financing.rawValue)
+//                        Text("Status: "+card.Status.rawValue)
+//                        Spacer()
+//                    }.padding([.leading,.top,.bottom]).font(.callout)
+//
+//                    VStack(alignment: .leading){
+//                        Text("Total Price: $"+String(card.OrderedBaby.TotalPrice))
+//                    }.padding(.trailing).font(.footnote)
+//                }
+//            }.background(Color.black).foregroundColor(Color.white).cornerRadius(10.0).matchedGeometryEffect(id: "card", in: cardspace).onTapGesture {
+//                expanded.toggle()
+//            }
+//        }
     }
-}
 
 struct coolTitle: View {
     var text: String
